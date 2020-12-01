04c9ca10825c1d63374674285c1f51c2ab40ac1b
technofille <ssenbol@gmail.com>
2020-12-01 16:35:37 +0200

    Initial preparation

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,16 @@
+﻿public class GameState
+{
+    public enum State
+    {
+        Start,
+        OnPlay,
+        Paused,
+        GameOver,
+    }
+
+    private int score = 0;
+    private State currentState = State.Start;
+
+    public int Score { get { return score; } set { score = value; } }
+    public State CurrentState { get { return currentState; } set { currentState = value; } }
+}
